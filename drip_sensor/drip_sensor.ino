e0d947ed41030444f821129fa85c1276564b999a
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-14 19:04:36 +0100

    Complete the Arduino code for the project

---
+++ drip_sensor/drip_sensor.ino
@@ -0,0 +1,122 @@
+#include <ESP8266WiFi.h>
+#include <Arduino.h>
+
+
+// Define GPIO pins for control
+const int s1g = D0;
+const int s1r = D1;
+const int s2g = D2;
+const int s2r = D6;
+const int buzz = D8;
+const int s3g = 1;
+const int s3r = 3;
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+    int id;
+    int x;
+    } struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// Create a structure to hold the readings from each board
+struct_message board1;
+struct_message board2;
+
+// Create an array with all the structures
+struct_message boardsStruct[2] = {board1, board2};
+
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsStruct[myData.id-1].x = myData.x;
+  delay(1000);
+  Serial.printf("x value: %d \n", boardsStruct[myData.id-1].x);
+  Serial.println();
+}
+ 
+void setup() {
+  pinMode(s1g, OUTPUT);
+  pinMode(s1r, OUTPUT);
+  pinMode(s2g, OUTPUT);
+  pinMode(s2r, OUTPUT);
+  pinMode(s3g, OUTPUT);
+  pinMode(s3r, OUTPUT);
+  pinMode(buzz, OUTPUT);
+
+
+
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
+
+  
+}
+
+void loop()
+{
+  // Access the variables for each board
+  int board1X = boardsStruct[0].x;
+  int board2X = boardsStruct[1].x;
+  int space1 = board1X;
+  int space2 = board2X;
+  
+  if (space1 > 3 && space1 < 10) {
+    // Turn on the red LED (s1r) and turn off the green LED (s1g)
+    digitalWrite(s1r, HIGH);
+    digitalWrite(s1g, LOW);
+    digitalWrite(buzz, HIGH);
+  } else if (space1 >= 11) {
+    // Turn on the green LED (s1g) and turn off the red LED (s1r)
+    digitalWrite(s1g, HIGH);
+    digitalWrite(s1r, LOW);
+    digitalWrite(buzz, LOW);
+  } 
+  
+
+  else if (space2 > 3 && space2 < 10) {
+    // Turn on the red LED (s1r) and turn off the green LED (s1g)
+    digitalWrite(s2r, HIGH);
+    digitalWrite(s2g, LOW);
+    digitalWrite(buzz, HIGH);
+  } else if (space2 >= 11) {
+    // Turn on the green LED (s1g) and turn off the red LED (s1r)
+    digitalWrite(s2g, HIGH);
+    digitalWrite(s2r, LOW);
+    digitalWrite(buzz, LOW);
+  } 
+  
+  
+  else {
+    // Turn off both LEDs when b is zero
+    digitalWrite(s1r, LOW);
+    digitalWrite(s1g, LOW);
+    digitalWrite(buzz, LOW);
+    digitalWrite(s2r, LOW);
+    digitalWrite(s2g, LOW);
+  
+  }
+ }
---
+++ drip_sensor_part/drip_sensor_part.ino
@@ -0,0 +1,112 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+//#include <BlynkSimpleEsp8266.h>
+#include "HX711.h"
+/*
+#define BLYNK_TEMPLATE_ID "TMPL2jtIIdMBI"
+#define BLYNK_TEMPLATE_NAME "Intravenous Monitor"
+#define BLYNK_AUTH_TOKEN "lXHQJWT8Ofjiqn3KYEpo2BNbMHvPehOI"
+
+BlynkTimer timer;
+char auth[] = BLYNK_AUTH_TOKEN;
+*/
+// REPLACE WITH RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0X58, 0XBF, 0X25, 0XD7, 0X52, 0X30};
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 0;
+const int LOADCELL_SCK_PIN = 2;
+
+HX711 scale;
+int reading;
+int lastReading;
+// REPLACE WITH YOUR CALIBRATION FACTOR
+
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+    int id;
+    int x;
+    } struct_message;
+
+// Create a struct_message called test to store variables to be sent
+struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+// Callback when data is sent
+//void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("\r\nLast Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  } 
+  // Set ESP-NOW role
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+
+  // Once ESPNow is successfully init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+
+//Blynk.begin(auth, ssid, pass);
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(CALIBRATION_FACTOR);   // this value is obtained by calibrating the scale with known weights
+  scale.tare();
+}
+ 
+void loop() {
+  if (scale.wait_ready_timeout(200)) {
+    reading = round(scale.get_units());
+    Serial.print("Weight: ");
+    Serial.println(reading);
+
+    lastReading = reading;
+
+  } else {
+    Serial.println("HX711 not found.");
+  }
+  
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+    myData.id = BOARD_ID;
+    myData.x = lastReading;
+    
+    // Send message via ESP-NOW
+    esp_now_send(0, (uint8_t *) &myData, sizeof(myData));
+    lastTime = millis();
+  }
+//int working = lastReading;
+delay(2000);
+/*  Blynk.run();
+  Blynk.virtualWrite(V0,lastReading);
+  delay(3000);*/
+}
+
